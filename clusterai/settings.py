8aa381f916d29f31b3d872351d35b6b2e1d115a6
Himanshujha2106 <himanshu.jha@ssipmt.com>
2024-03-12 14:40:48 +0530

    1th

--- clusterai/settings.py
+++ clusterai/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8v@71(c^7+oxvrqzqk=)$ohp*-7yt@5p-*xg(kw28&r@56d7(r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
